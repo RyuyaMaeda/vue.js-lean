db0033ed9fc4d15994f24c530eff9c63b32bfba3
RyuyaMaeda <ryuya.maeda@esm.co.jp>
2020-09-15 13:51:38 +0900

    add: 親コンポーネントに子コンポーネントを含めるようにした

---
+++ my-project/src/App.vue
@@ -0,0 +1,20 @@
+<!--scopedはtempalte内は、キーエレメントがないといけないため、<div>などでラップする必要がある-->
+<template>
+  <div>
+    <Header></Header>
+    <Body></Body>
+    <Body></Body>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Body from './components/Body.vue'
+
+export default {
+  components: {
+    Header,
+    Body
+  }
+}
+</script>
