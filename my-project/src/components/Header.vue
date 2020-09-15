834a51960ae388c09b9e0429b1c87de443333cab
RyuyaMaeda <ryuya.maeda@esm.co.jp>
2020-09-15 13:28:42 +0900

    add: 新しくコンポーネントを作成した

---
+++ my-project/src/components/Header.vue
@@ -0,0 +1,31 @@
+<!--scopedはtempalte内は、キーエレメントがないといけないため、<div>などでラップする必要がある-->
+<template>
+  <div>
+    <h1>{{ title}}</h1>
+    <p>{{text}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // VueコンポーネントはVueインスタンスの拡張であるので、オブジェクト型で定義できない。代わりに関数定義する必要がある。
+  data() {
+    return {
+      title: "Header",
+      text: "Hello Vue.js!"
+    }
+  }
+}
+</script>
+<!--scopedは「このコンポーネントにのみ、styleを適用する」という意味-->
+<style scoped>
+div{
+  border: 1px solid bblue
+}
+h1{
+  color: blue
+}
+p{
+  color:blue
+}
+</style>
