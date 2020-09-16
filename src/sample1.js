3657458e2c015274ef0ad2113809ab1971e19f5c
前田竜弥 <naeda@MacBook-Pro.local>
2020-09-16 22:35:43 +0900

    add : to doアプリを作成した

--- src/sample1.js
+++ src/sample1.js
@@ -1,12 +1,34 @@
 var app = new Vue({
   el: '#app',
   data: {
-    msg1: 'はじめてのVue.js',
-    msg2: 'Vue.js'
+    storageKey: 'todolist',
+    newtodo: '',
+    todolist: []
   },
   methods: {
-    changeMsg1: function (name) {
-      this.msg1 = 'こんにちは' + name
+    addTodo: function () {
+      if (this.newtodo === '') { return; }
+      this.todolist.push({ text: this.newtodo, done: false, hover: false });
+      this.newtodo = '';
+    },
+    remove: function (index) {
+      if (this.todolist[index].done === true) {
+        this.todolist.splice(index, 1);
+      }
+    },
+  },
+  created: function () {
+    var dataStr = localStorage.getItem(this.storageKey);
+    if (dataStr) {
+      this.todolist = JSON.parse(dataStr);
     }
-  }
-})
+  },
+  watch: {
+    todolist: function () {
+      localStorage.setItem(
+        this.storageKey,
+        JSON.stringify(this.todolist)
+      )
+    }
+  },
+});
