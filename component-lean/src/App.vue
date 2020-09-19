f6c405265268298ef55ece7df5a25105a52d4f6c
前田竜弥 <naeda@MacBook-Pro.local>
2020-09-19 17:34:08 +0900

    add: おみくじで運勢を占えるようにした

--- component-lean/src/App.vue
+++ component-lean/src/App.vue
@@ -1,46 +1,65 @@
 <!--scopedはtempalte内は、キーエレメントがないといけないため、<div>などでラップする必要がある-->
 <template>
-  <div>
+  <div id="app">
+    <p>今年は{{ (new Date()).getFullYear() }}年</p>
+    <p>
+      <a v-bind:href="toEsm">永和のサイトへ</a>
+    </p>
     <!--子コンポーネントを呼び出しているタグの中でカスタム属性を定義することができる-->
-    <Header :username='name' :inputvalue="condition" @inputvalue="changeConditionValue"></Header>
+    <Header :username="name" :inputvalue="condition" @inputvalue="changeConditionValue"></Header>
     <!--子コンポーネントで定義したaddイベントは、親コンポーネント側のタグ属性で@add=[関数名]と記載することで、methodsで定義してある関数に繋げることができます。-->
     <Body @add="add1"></Body>
     <Body @add="add2"></Body>
     <button @click="changeComponentName('Header')">Headerに変更する</button>
     <button @click="changeComponentName('Body')">Bodyに変更する</button>
     <keep-alive>
-    <component :is="componentName"></component>
+      <component :is="componentName"></component>
     </keep-alive>
-    <input type="text" v-model="condition" >
+    <input type="text" v-model="condition" />
     <p>total: {{ totalcount }}</p>
+    <button @click="uranau()">今日の運勢を占う</button>
+    <div><img :="imgAttributes" /></div>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Body from './components/Body.vue'
+import Header from "./components/Header.vue";
+import Body from "./components/Body.vue";
 
 export default {
   data() {
     return {
-      name: "naeda",
+      name: "前田",
+      toEsm: "https://www.esm.co.jp/",
       count1: 0,
       count2: 0,
       totalcount: 0,
       condition: "aaaa",
-      componentName: "Header"
-    }
+      componentName: "Header",
+      kujis: [
+        require("@/img/omikuji_syoukichi.png"),
+        require("@/img/omikuji_chuukichi.png"),
+        require("@/img/omikuji_daikichi.png"),
+        require("@/img/omikuji_kyou.png"),
+      ],
+      imgAttributes: {
+        src: require("@/img/omikuji_syoukichi.png"),
+        width: "300",
+        height: "200",
+        alt: "おみくじ",
+      },
+    };
   },
   components: {
     Header,
-    Body
+    Body,
   },
   methods: {
-    add1(count){
+    add1(count) {
       this.count1 = count;
       this.totalcount = this.count1 + this.count2;
     },
-    add2(count){
+    add2(count) {
       this.count2 = count;
       this.totalcount = this.count1 + this.count2;
     },
@@ -49,7 +68,12 @@ export default {
     },
     changeConditionValue(inputValue) {
       this.condition = inputValue;
-    }
-  }
-}
+    },
+    uranau() {
+      this.imgAttributes.src = this.kujis[
+        Math.floor(Math.random() * this.kujis.length)
+      ];
+    },
+  },
+};
 </script>
--- component-lean/src/components/Body.vue
+++ component-lean/src/components/Body.vue
@@ -3,7 +3,10 @@
   <div>
     <h2>{{ title}}</h2>
     <p>{{text}}</p>
-    <p><button @click="increment">+1</button> {{ count }} </p>
+    <p>
+      <button @click="increment">+1</button>
+      {{ count }}
+    </p>
   </div>
 </template>
 
@@ -14,27 +17,27 @@ export default {
     return {
       title: "Body",
       text: "Have a good day!",
-      count: 0
-    }
+      count: 0,
+    };
   },
   methods: {
-    increment(){
+    increment() {
       this.count += 1;
       // 第一引数でaddイベントを定義して、第二引数でコンポーネントの値を引数として渡す
-      this.$emit("add",this.count);
-    }
-  }
-}
+      this.$emit("add", this.count);
+    },
+  },
+};
 </script>
 <!--scopedは「このコンポーネントにのみ、styleを適用する」という意味-->
 <style scoped>
-div{
-  border: 1px solid red
+div {
+  border: 1px solid red;
 }
-h1{
-  color: red
+h1 {
+  color: red;
 }
-p{
-  color: red
+p {
+  color: red;
 }
 </style>
