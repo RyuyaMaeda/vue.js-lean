a3ff7f537ae10a3e5354512676dfcc77612d437a
前田竜弥 <naeda@MacBook-Pro.local>
2020-09-20 23:24:12 +0900

    add: その他諸々のディレクティブの使い方を試した

--- component-lean/src/App.vue
+++ component-lean/src/App.vue
@@ -2,6 +2,9 @@
 <template>
   <div id="app">
     <p>今年は{{ (new Date()).getFullYear() }}年</p>
+    <p>只今の時間(算出プロパティ)は{{ dataProp}}</p>
+    <p>只今の時間(メソッド)は{{ dataMethod() }}</p>
+
     <p>
       <a v-bind:href="toEsm">永和のサイトへ</a>
     </p>
@@ -10,16 +13,40 @@
     <!--子コンポーネントで定義したaddイベントは、親コンポーネント側のタグ属性で@add=[関数名]と記載することで、methodsで定義してある関数に繋げることができます。-->
     <Body @add="add1"></Body>
     <Body @add="add2"></Body>
+    <!--ボタンを押して表示させるコンポーネントを変える-->
     <button @click="changeComponentName('Header')">Headerに変更する</button>
     <button @click="changeComponentName('Body')">Bodyに変更する</button>
     <keep-alive>
       <component :is="componentName"></component>
     </keep-alive>
+    <!--双方向バインディング-->
     <input type="text" v-model="condition" />
     <p>total: {{ totalcount }}</p>
+    <!--v-bindを使ったclass属性の変更-->
     <button @click="uranau()">今日の運勢を占う</button>
-    <div :style="myStyle"><img :="imgAttributes" /></div>
+    <table>
+      <th>くじの結果</th>
+      <th>結果の詳細</th>
+      <tr v-for="(detail, kujiname) in kujiDetail" :key="kujiname">
+        <td>{{ kujiname }}</td>
+        <td>{{detail}}</td>
+      </tr>
+    </table>
+    <div
+      :class="{center: position.center, left: position.left, right: position.right}"
+    ><img :="imgAttributes" v-if="show"/></div>
+    <!--ボタンを使った画像表示非表示の切り替え-->
+    <button @click="show = !show">画像の表示を変える</button>
     <button @click="changeStyle">画像の配置を中央に変える</button>
+    <br />年齢を入力してください
+    <input type="text" v-model="age" />
+    <p v-if="this.age >= 20">お酒は飲めます</p>
+    <p v-else-if="this.age <20">お酒は飲めません</p>
+    <p v-else>年齢を入力してください</p>
+    <ul>
+      <li v-for="(number,index) in colors" :key="index">{{ number}}</li>
+    </ul>
+    <button @click="colors[2] = 9">値を変更</button>
   </div>
 </template>
 
@@ -31,11 +58,12 @@ export default {
   data() {
     return {
       name: "前田",
+      age: "",
       toEsm: "https://www.esm.co.jp/",
       count1: 0,
       count2: 0,
       totalcount: 0,
-      condition: "aaaa",
+      condition: "",
       componentName: "Header",
       kujis: [
         require("@/img/omikuji_syoukichi.png"),
@@ -49,12 +77,30 @@ export default {
         height: "200",
         alt: "おみくじ",
       },
-      myStyle: {},
+      position: {
+        left: true,
+        center: false,
+        right: false,
+      },
+      show: true,
+      kujiDetail: {
+        daikiti: "運勢はかなり良いです",
+        tyuukiti: "運勢はそこそこ良いです",
+        syoukiti: "運勢は普通です",
+        kyou: "運勢は悪いです",
+      },
+      colors: ["blue", "green", "red"],
     };
   },
   components: {
     Header,
     Body,
+  },
+  computed: {
+    dataProp() {
+      console.log(this.condition);
+      return new Date().toLocaleString();
+    },
   },
   methods: {
     add1(count) {
@@ -77,9 +123,26 @@ export default {
       ];
     },
     changeStyle() {
-      this.myStyle.width = "30%";
-      this.myStyle.margin = "auto";
+      this.position.left = false;
+      this.position.center = true;
+    },
+    dataMethod() {
+      return new Date().toLocaleString();
     },
   },
 };
 </script>
+<style>
+.left {
+  width: 30%;
+  margin-left: 0px;
+}
+.center {
+  width: 30%;
+  margin: auto;
+}
+.right {
+  width: 30%;
+  margin-right: 0px;
+}
+</style>
